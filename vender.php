a6363a39312e85e864542aeb76db2768b3d0d146
Jaqueline Galindo Huitron <143548375+JaquelineGalindoHuitron@users.noreply.github.com>
2023-11-17 23:18:45 -0700

    Add files via upload

---
+++ vender.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+include_once "encabezado.php";
+if (!isset($_SESSION["carrito"])) $_SESSION["carrito"] = [];
+$granTotal = 0;
+?>
+<div class="col-xs-12">
+	<h1>Vender</h1>
+	<?php
+	if (isset($_GET["status"])) {
+		if ($_GET["status"] === "1") {
+	?>
+			<div class="alert alert-success">
+				<strong>¡Correcto!</strong> Venta realizada correctamente
+			</div>
+		<?php
+		} else if ($_GET["status"] === "2") {
+		?>
+			<div class="alert alert-info">
+				<strong>Venta cancelada</strong>
+			</div>
+		<?php
+		} else if ($_GET["status"] === "3") {
+		?>
+			<div class="alert alert-info">
+				<strong>Ok</strong> Servicio quitado de la lista
+			</div>
+		<?php
+		} else if ($_GET["status"] === "4") {
+		?>
+			<div class="alert alert-warning">
+				<strong>Error:</strong> El servicio que buscas no existe
+			</div>
+		<?php
+		} else if ($_GET["status"] === "5") {
+		?>
+			<div class="alert alert-danger">
+				<strong>Error: </strong>El servicio está agotado
+			</div>
+		<?php
+		} else {
+		?>
+			<div class="alert alert-danger">
+				<strong>Error:</strong> Algo salió mal mientras se realizaba la venta
+			</div>
+	<?php
+		}
+	}
+	?>
+	<br>
+	<form method="post" action="agregarAlCarrito.php">
+		<label for="modelo">Servicio:</label>
+		<input autocomplete="off" autofocus class="form-control" name="servicio" required type="text" id="servicio" placeholder="Escribe el ID Servicio">
+	</form>
+	<br><br>
+	<table class="table table-bordered">
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Servicio</th>
+				<th>Descripción</th>
+				<th>ID empleado</th>
+				<th>Dirección</th>
+				<th>Telefono</th>
+				<th>Precio</th>
+				<th>Disponible</th>
+				<th>Cantidad</th>
+				<th>Total</th>
+				<th>Quitar</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($_SESSION["carrito"] as $indice => $servicio) {
+				$granTotal += $servicio->total;
+			?>
+				<tr>
+				<td><?php echo $servicio->id ?></td>
+					<td><?php echo $servicio->nomServicio ?></td>
+					<td><?php echo $servicio->descripcion ?></td>
+					<td><?php echo $servicio->idempleado ?></td>
+					<td><?php echo $servicio->direccion ?></td>
+					<td><?php echo $servicio->telefono ?></td>
+					<td><?php echo $servicio->precio ?></td>
+					<td><?php echo $servicio->disponible ?></td>
+					<td>
+						<form action="cambiar_cantidad.php" method="post">
+							<input name="indice" type="hidden" value="<?php echo $indice; ?>">
+							<input min="1" name="cantidad" class="form-control" required type="number" step="1" value="<?php echo $servicio->cantidad; ?>">
+						</form>
+					</td>
+					<td><?php echo $servicio->total ?></td>
+					<td><a class="btn btn-danger" href="<?php echo "quitarDelCarrito.php?indice=" . $indice ?>"><i class="fa fa-trash"></i></a></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+
+	<h3>Total: <?php echo $granTotal; ?></h3>
+	<form action="./terminarVenta.php" method="POST">
+		<input name="total" type="hidden" value="<?php echo $granTotal; ?>">
+		<button type="submit" class="btn btn-success">Terminar venta</button>
+		<a href="./cancelarVenta.php" class="btn btn-danger">Cancelar venta</a>
+	</form>
+</div>
+<?php include_once "pie.php" ?>
